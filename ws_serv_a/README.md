2e40c6c9ab627edefdf1669d8f1bc47a7025b244
cmammer <129841175+cmammer@users.noreply.github.com>
2023-04-04 13:43:56 +0300

    Add files via upload

---
+++ ws_serv_a/README.md
@@ -0,0 +1,17 @@
+### Installation
+```
+cd ~/catkin_ws/src/
+git clone https://github.com/voltbro/ws_serv_a.git 
+```
+### Compilation
+```
+cd ~/catkin_ws/
+catkin_make --pkg ws_serv_a
+```
+### Usage
+
+Just run configure.launch
+```
+roslaunch ws_serv_a start_configure_A.launch
+```
+
---
+++ ws_serv_a/scripts/service_configuration.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import time
+import tqdm
+# import rospy
+
+configuration_number = 5015
+version = "1.0.0"
+
+print("Service package 1: ver. {}".format(version))
+time.sleep(0.8)
+print("Service package 1: start configuration ")
+time.sleep(0.5)
+for i in tqdm.tqdm(range(7500), ascii=True, desc="System check"):
+    time.sleep(0.001)
+
+print("Service package 1: successfully configured!")
+time.sleep(0.5)
+print("Service package 1: configuration checksum : {}".format(configuration_number))
+print("Service package 1: please push Ctrl+C to finish")
